b4acabfc70ec0b52d26f7a22ca717c1af5cd4ea8
barnayjakrizel-sys <barnayjakrizel@gmail.com>
2026-07-29 14:30:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -698,7 +698,36 @@ body{
 .popup-buttons{
 
     flex-direction:column;
+// =============================
+// Background Music
+// =============================
 
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+let playing = false;
+
+musicBtn.addEventListener("click", () => {
+
+    if(!playing){
+
+        music.play();
+
+        musicBtn.innerHTML = "⏸ Pause Music";
+
+        playing = true;
+
+    }else{
+
+        music.pause();
+
+        musicBtn.innerHTML = "🎵 Play Music";
+
+        playing = false;
+
+    }
+
+});
 }
 
 }
