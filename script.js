9cb17fdf5fb5327a6ff1983548da36dafe1c688c
barnayjakrizel-sys <barnayjakrizel@gmail.com>
2026-07-29 11:27:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const startBtn = document.getElementById("startBtn");
 
+startBtn.addEventListener("click", () => {
+
+    alert("❤️ Welcome to our love story, Beb ❤️\n\nThis is just the beginning...");
+
+});
